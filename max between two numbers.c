e057b5b5819615d157975826b518557e07a5143b
Pratik-SD2040 <pratik.software2040@gmail.com>
2025-11-02 09:24:24 +0530

    Add files via upload

---
+++ max between two numbers.c
@@ -0,0 +1,16 @@
+int main(){
+    float num1,num2;
+    
+    printf("Enter number1 :");
+    scanf("%f",&num1);
+    printf("Enter number2 :");
+    scanf("%f",&num2);
+    
+        if(num1>num2){
+            printf("Number1>Number2");
+        }else{
+            printf("Number2>Number1");
+        }
+        
+return 0;
+}
