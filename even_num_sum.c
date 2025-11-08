5c8190bb74dbf95571d10d73d9a074fc42cc0f02
Pratik <pratik.software2040@gmail.com>
2025-11-08 11:17:58 +0530

    Add files via upload

---
+++ even_num_sum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+    int n, sum;
+
+    printf("Enter the value of n.(n is an odd number) :");
+    scanf("%d",&n);
+
+    sum=n(n+1);
+
+    printf("sum =%d",sum);
+
+    getch();
+    return 0;
+}
